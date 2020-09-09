dc494142be3a5b932e43a73d21129023128be61a
icarusinnovation <70998795+icarusinnovation@users.noreply.github.com>
2020-09-09 21:03:58 +0530

    init

---
+++ js/projlist.js
@@ -0,0 +1,96 @@
+//type List = work, personalprojects, weddings
+
+projListFill([
+{
+"title": "Shriya & Karthik",
+"desc": "Wedding Invite Design & Stationery",
+"type": "weddings",
+"url": "shriya_karthik.jpg",
+"folder" : "Shriya _ Karthik"
+},
+{
+"title": "Chaandi",
+"desc": "Illustration & Merchandise",
+"type": "work",
+"url": "chaandi.jpg",
+"folder" : "Chaandi"
+},
+{
+"title": "Akane",
+"desc": "Illustration",
+"type": "work",
+"url": "Akane.gif",
+"folder" : "Akane _ Social Media Campaign"
+},
+{
+"title": "Bhavya Ramesh",
+"desc": "Branding & Identity Design",
+"type": "work",
+"url": "VC_mockup.jpg",
+"folder": "Bhavya Ramesh"
+},
+{
+"title": "Muskan & Chirag",
+"desc": "Wedding Invite Design & Stationery",
+"type": "weddings",
+"url": "C+M_MOCKUPS-06.jpg",
+"folder": "Muskan _ Chirag"
+},
+{
+"title": "Priyanka & Yash",
+"desc": "Wedding Invite Design & Stationery",
+"type": "weddings",
+"url": "IMG_0244.JPG",
+"folder": "Priyanka _ Yash"
+},
+{
+"title": "Icarus",
+"desc": "Branding & Identity Design",
+"type": "work",
+"url": "Icarus_Spot-UV-Logo-MockUp.jpg",
+"folder": "Icarus"
+},
+{
+"title": "Komal & Kritish",
+"desc": "Wedding Invite Design",
+"type": "weddings",
+"url": "KK_MOCKUP-04.jpg",
+"folder" : "Komal _ Kritish"
+},
+{
+"title": "Vishal & Nidhi",
+"desc": "Wedding Invite Design",
+"type": "weddings",
+"url": "VishalNidhi_Inserts-06.jpg",
+"folder" : "Vishal _ Nidhi"
+},
+{
+"title": "Vintage Botanicals",
+"desc": "Illustration & Stationery",
+"type": "personalprojects",
+"url": "bananatree.jpg",
+"folder" : "Vintage Botanicals"
+},
+{
+"title": "Stippled Stories",
+"desc": "Illustration",
+"type": "personalprojects",
+"url": "Monstera_SG.png",
+"folder": "Stippled Stories"
+},
+{
+"title": "36 Days of Type - 2018 Edition",
+"desc": "Illustration",
+"type": "personalprojects",
+"url": "R.jpg",
+"folder" : "36 days of type 2018 edition"
+},
+{
+"title": "Surface Pattern Design for We Are Premiere Vision (Paris)",
+"desc": "Illustration & Merchandise",
+"type": "work",
+"url": "Surface pattern_social media-06.jpg",
+"folder" : "Surface Pattern Design"
+},
+
+])
---
+++ project/js/projMap.js
@@ -0,0 +1,108 @@
+var projMap = 
+{
+
+	"Bhavya Ramesh":{
+		'title': "Bhavya Ramesh",
+		'desc': "Bhavya Ramesh is a unique silver jewelry brand with a line of products strongly defined by natural elements. Bold and edgy are two words to describe the brand.\
+We designed a minimal logo with complimenting colours of red and grey. We have incorporated an upward arrow in the logo, inspired by vintage Indian metal ornaments & an arrow being synonymous for movement and structure, much like the jewelry Bhavya Ramesh creates.",
+		'length' : 8,
+		'giflength' : 1,
+	},
+
+	"Muskan _ Chirag":{
+		'title': "Muskan & Chirag",
+		'desc': "Inspired by palatial opulence, this wedding invite was designed for a traditional affair. The wedding venue being Mysore, a lot of inspiration was drawn from tropical foliage & intricate details of a South Indian Palace, christened with the grandeur of quintessential Mysore elephants.\
+ We designed the invite card with inserts and an envelope, stationery for the couple and a few giveaways at the wedding.",
+		'length' : 10
+	},
+
+	"Priyanka _ Yash":{
+		'title': "Priyanka & Yash",
+		'desc': "A Rajasthani wedding, hence a lot of inspiration was drawn from the checkered and jaali patterns which usually ornament a traditional Rajasthani door. As the wedding took place in spring and was by the beach, the colour palette is different pastel shades, adorned with the bride and grooms favourite flowers (the mogra and the rose). The design was a mix of traditional and modern.\
+		The same uniformity was then followed in the stationery design as well.",
+		'length' : 17
+	},
+
+	"Icarus":{
+		'title': "Icarus",
+		'desc': "Branding & Identity deisgned for a Software Development firm, that primarily deals in the programming of drones for industrial use and its functions. Icarus means aritificial wings, this definition reflects in the logo form and branding as well.",
+		'length' : 5,
+		'giflength' : 1,
+	},
+
+	"Komal _ Kritish":{
+		'title': "Komal & Kritish",
+		'desc': "The couple went for a simple wedding invite. They were particular about wedding elements adorning this minimal but colourful card. We have created a minimal Indian wedding invite, with Indian patterns across the invite cards.",
+		'length' : 6
+	},
+
+	"Vishal _ Nidhi":{
+		'title': "Vishal & Nidhi",
+		'desc': "A fun take on Indian wedding functions, this was a completely illustrated invite, which was digitally sent out to all the guests at the venue. The colours are bright representing the big fat Indian wedding. The illustrations are hand drawn, inspired by real wedding events. We had a total blast designing the e-invite and stationery for this wedding.",
+		'length' : 8
+	},
+
+	"Vintage Botanicals":{
+		'title': "Vintage Botanicals",
+		'desc': "Illustrated vintage botanicals for a client who had a penchant for tropical stationery. These were digitally created and designed to suit the customised stationery set.",
+		'length' : 6
+	},
+
+
+	"Stippled Stories":{
+		'title': "Stippled Stories",
+		'desc': "A bunch of illustrations heavily influenced by sureealism in a mundane world.",
+		'length' : 5,
+		'giflength' : 1,
+	},
+
+	"36 days of type 2018 edition":{
+		'title': "36 Days Of Type 2018 Edition",
+		'desc': "An online exercise that takes place every year thru Instagram. The theme followed here was Indian flowers and the typeface was created around it.",
+		'length' : 18
+	},
+
+	"Surface Pattern Design":{
+		'title': "Surface Pattern Design",
+		'desc': "Created a surreal surface pattern in a pen illustrated-stippled style, which was displayed at the prestigious, Premiere Vision (Paris). This exhibition is centered around fashion and these prints were eventually going to be a part of merchandise.",
+		'length' : 7
+	},
+	
+	
+	"Chaandi":{
+		'title': "Chaandi",
+		'desc': "Chaandi is an online siver jewelry store. While creating their identity & branding, we have played with the word 'chaandi'which means silver and 'moon-like'. The logo form follows the crescent shape of the moon and also takes reference from a popular jewel shape,the 'chaandbali'.",
+		'length' : 11,
+		'giflength' : 1,
+	},
+
+	"Akane _ Social Media Campaign": {
+		'title': "Akane",
+		'desc': "Akane works with their immediate natural surroundings to dye on eco-friendly cloth. Their processes and end products are sustainable and beneficial to the user and the environment. We illustrated a few natural flowers and roots that they use while dyeing their products.",
+		'length' : 6,
+		'giflength' : 1,
+	},
+
+
+	"Shriya _ Karthik": {
+		'title': "Shriya & Karthik",
+		'desc': "Sangam, a confluence of two souls. Designed a wedding invite for a South Indian Couple. The colours and forms of the hand drawn illustrations on the invite are inspired from Pichwai paintings and their settings.<br /><br /> \
+		<div style='font-family: didotItalic; font-weight: bold;text-align: left; padding: 0 2rem;'>' The wide skies are welcoming, holding unsaid emotions together.<br />\
+		Perched parrots watch with glee, talkative, like colours in motion.<br />\
+		Banana leaves sway dancing slowly, to the song of the season.<br />\
+		The marigolds & the mango leaves, looking their best, join the occasion.<br />\
+		The sweet smell of mogra fills the air, tied together in union.<br />\
+		The low hum of drums, the rhythmic sound of the sitar,<br />\
+		Declaring it’s a celebration.'</div>",
+
+		'length' : 18,
+		'giflength' : 2,
+	},
+
+
+
+}
+
+
+
+
